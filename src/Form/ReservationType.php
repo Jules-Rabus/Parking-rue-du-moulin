8531a536296bb4e5020e69878144343f4bc2ebba
uid175430 <uid175430@h2web256.infomaniak.ch>
2022-03-22 16:58:34 +0100

    Amélioration css et formulaire réservation

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -3,12 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Reservation;
+use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ReservationType extends AbstractType
 {
@@ -16,20 +18,32 @@ class ReservationType extends AbstractType
     {
         $builder
             ->add('NombrePlace',IntegerType::class,[
-                'attr'=>['min'=>0, 'max'=>10]
+                'label' => 'Nombre de Place',
+                'required' => 'true',
+                'attr' =>['min'=>1, 'max'=>10, 'value' => 1]
             ])
             ->add('DateArrivee', DateType::class,[
+                'label' => 'Date d\'arrivée',
                 'widget' => 'single_text',
                 'data' => $options['date'],
+                'required' => 'true',
                 'attr'=>['min'=>$options['date']->format('Y-m-d')]
             ])
             ->add('DateDepart', DateType::class,[
+                'label' => 'Date de départ',
                 'widget' => 'single_text',
                 'data' => $options['date'],
+                'required' => 'true',
                 'attr'=>['min'=>$options['date']->format('Y-m-d')]
             ])
-            ->add('Telephone',TelType::class)
-            ->add('Client')
+            ->add('Telephone',TelType::class, [
+                'label' => 'Téléphone',
+                'required' => 'true',
+                'attr' => ['placeholder'=> ' Numéro de Téléphone']])
+            ->add('Client',EntityType::class,[
+                'class' => Client::class,
+                'required' => 'false'
+            ])
         ;
     }
 
